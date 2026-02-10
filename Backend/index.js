79ce55390bc0f8327e90e0d6181c20488e70440d
Lina-alsulami <magid1400@icloud.com>
2026-02-10 18:56:38 +0300

    Configure dynamic port for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -95,10 +95,14 @@ app.put('/tasks/:id', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
+const PORT = process.env.PORT || 3000; 
+
 sequelize.sync().then(() => {
     console.log("Database connected and table created!");
-    app.listen(3000, () => {
-        console.log("Server is running on http://localhost:3000");
+    app.listen(PORT, () => {
+        
+        console.log(`Server is running on port ${PORT}`);
     });
 }).catch(err => {
     console.error("Unable to connect to the database:", err);
